ef2102d49498a0e79ead75acd5b0e63a7d89e6b7
mided3v <ayo4real6@gmail.com>
2022-11-13 23:27:01 +0100

    navbar content

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import Logo from "./../../asset/metabnb-logo.png"
+import navStyles from './Navbar.css'
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+          <div className="logo">
+              <a href="#">
+                  <img src={Logo} alt="" />
+        </a>
+      </div>
+
+      <ul className="nav-options">
+        <li>Home</li>
+        <li>Place to stay</li>
+        <li>NFTs</li>
+        <li>Community</li>
+      </ul>
+
+      <div className="wallet-btn">
+        <a href="#">Connect wallet</a>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
